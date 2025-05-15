7d7c244d1ea2ca0621d669d7c59f4a24e4960164
GigikerJD <nelgie.ferrol94@gmail.com>
2025-05-15 14:56:06 +0100

    affichage films selon leur ville

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,75 @@
-import { ButtonGroup, Button, Container, Link, Typography, Box } from "@mui/material"
-import { AppAuth } from "../config/AppProvider"
-
+import {
+  ButtonGroup,
+  Button,
+  Container,
+  Link,
+  Typography,
+  Box,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { AppAuth } from "../config/AppProvider";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const Home = () => {
+  const { backendUrl } = AppAuth();
+  const [movies, setMovies] = useState(null);
+  const [cities, setCities] = useState([]);
+  const [message, setMessage] = useState("");
 
-    return(
-        <>
-            <Container maxWidth="lg" sx={{ border: '1px solid lightblue'}}>
+  useEffect(() => {
+    async function getAllMovies() {
+      const response = await axios.get(backendUrl + "movies", {
+        validateStatus: (status) => {
+          return true;
+        },
+      });
+      setMovies(response.data);
+    }
+    getAllMovies();
+  }, [backendUrl]);
+
+  useEffect(() => {
+    async function getCities() {
+      const response = await axios.get(backendUrl + "movies/cities", {
+        validateStatus: (status) => {
+          return true;
+        },
+      });
+      const type = response.data.type;
+      switch (type) {
+        case "error":
+          setMessage(response.data.message);
+          break;
+        default:
+          setCities(response.data.cities);
+          break;
+      }
+    }
+    getCities();
+  });
+
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ border: "1px solid lightblue" }}>
+        <Tabs 
+            visibleScrollbar 
+            variant="fullWidth"
+            centered
+            sx={{
                 
-            </Container>
-        </>
-    )
-}
+            }}
+        >
+          {cities.map((city, index) => (
+            <>
+              <Tab label={city} value={index} />
+            </>
+          ))}
+        </Tabs>
+
+        <Box component="pre">{JSON.stringify(movies, null, 2)}</Box>
+      </Container>
+    </>
+  );
+};
--- frontend/src/pages/Studios.jsx
+++ frontend/src/pages/Studios.jsx
@@ -3,46 +3,40 @@ import axios from "axios";
 import { AppAuth } from "../config/AppProvider";
 
 export const Studios = () => {
-    const { backendUrl } = AppAuth();
-    const [movies, setMovies] = useState(null);
-    const [message, setMessage] = useState("");
-    const [status, setStatus] = useState(0);
+  const { backendUrl } = AppAuth();
+  const [movies, setMovies] = useState(null);
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(0);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            const response = await axios.get(backendUrl + '/api/movies', {
-                    validateStatus: (status) =>{
-                        return true
-                    }
-                });
-                const status = response.status;
-                setStatus(status);
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get(backendUrl + "/api/movies", {
+        validateStatus: (status) => {
+          return true;
+        },
+      });
+      const status = response.status;
+      setStatus(status);
 
-                switch (status) {
-                    case 200:
-                        setMovies(response.data);
-                        break;
-                    case 404:
-                        setMessage(response.data.message);
-                        break;
-                    default:
-                        setMessage("An unexpected error occurred.");
-                }
-        };
+      switch (status) {
+        case 200:
+          setMovies(response.data);
+          break;
+        case 404:
+          setMessage(response.data.message);
+          break;
+        default:
+          setMessage("An unexpected error occurred.");
+      }
+    };
 
-        fetchMovies();
-    }, []);
+    fetchMovies();
+  }, []);
 
-    return (
-        <>
-            {status === 200 && movies && (
-                <pre>
-                    {JSON.stringify(movies, null, 2)}
-                </pre>
-            )}
-            {status === 404 && (
-                <span>{message}</span>
-            )}
-        </>
-    );
+  return (
+    <>
+      {status === 200 && movies && <pre>{JSON.stringify(movies, null, 2)}</pre>}
+      {status === 404 && <span>{message}</span>}
+    </>
+  );
 };
